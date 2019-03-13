8f79c370f62114ba67f75f06798064189e57412a
Jacob Coons <jacobtcoons@gmail.com>
2019-03-13 15:55:13 -0700

    filled landing page with current listings, set up form submission in create listing page

--- src/components/pages/create-listing-page.js
+++ src/components/pages/create-listing-page.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
+import '../pages-styling/create-listing-page.css';
 
-export function CreateListingPage(props) {
-  return (
+export default class CreateListingPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  formSubmitted(event) {
+    event.preventDefault();
+    let listing = {
+      title: this.titleInput.value,
+    //         // description: $("#create-description").val(),
+    //         // category: $("#create-category").val(),
+    //         // location: $("#create-location").val(),
+    //         // applyLink: $("#create-apply-link").val()
+    };
+    console.log(listing);
+  }
+  render() {
+    return (
     <div className="listing-container">
     	<nav role="navigation" className="nav-bar">
             <h1 className="site-logo">DG</h1>
@@ -12,28 +28,33 @@ export function CreateListingPage(props) {
             <h2 className="page-title">Create Listing</h2>
         </header>
     	<main role="main">
-    		<form>
-    			<label className="form-label">Title</label>
-    			<input type="text" required="true" className="form-input"></input>
-    			<label className="form-label">Description</label>
-    			<input type="text" required="true" className="form-input"></input>
-    			<label className="form-label">Category</label>
-    			<select className="form-input">
-    				<option value="#">#</option>
-    				<option value="#">#</option>
-    				<option value="#">#</option>
-    				<option value="#">#</option>
-    				<option value="#">#</option>
+    		<form id="create-listing-form" onSubmit={(event) => this.formSubmitted(event)}>
+    			<label className="form-label" htmlFor="create-title">Title</label>
+    			<input type="text" required="true" className="form-input" id="create-title" ref={input => this.titleInput = input}></input>
+    			<label className="form-label" htmlFor="create-description">Description</label>
+    			<input type="text" required="true" className="form-input" id="create-description"></input>
+    			<label className="form-label" htmlFor="create-category">Category</label>
+    			<select className="form-input" id="create-category">
+    				<option value="animals">Animals</option>
+    				<option value="arts-culture-humanities">Arts, Culture, Humanities</option>
+    				<option value="community-development">Community Development</option>
+    				<option value="education">Education</option>
+    				<option value="environment">Environment</option>
+                    <option value="health">Health</option>
+                    <option value="human-and-civil-rights">Human and Civil Rights</option>
+                    <option value="human-and-social-services">Human and Social Services</option>
+                    <option value="international">International</option>
+                    <option value="research-and-public-policy">Research and Public Policy</option>
+                    <option value="religion">Religion</option>
     			</select>
-    			<label className="form-label">location</label>
-    			<input type="text" required="true" className="form-input"></input>
-    			<label className="form-label">Apply link</label>
-    			<input type="text" required="true" className="form-input"></input>
+    			<label className="form-label" htmlFor="create-location">location</label>
+    			<input type="text" required="true" className="form-input" id="create-location"></input>
+    			<label className="form-label" htmlFor="create-apply-link">Apply link</label>
+    			<input type="text" required="true" className="form-input" id="create-apply-link"></input>
                 <button type="submit" className="button-style" id="create-button">Create</button>
     		</form>
     	</main>
     </div>
-  );
-}
-
-export default CreateListingPage;
+    );
+  }
+};
--- src/components/pages/landing-page.js
+++ src/components/pages/landing-page.js
@@ -3,33 +3,58 @@ import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import '../pages-styling/landing-page.css';
 
-export default function LandingPage(props) {
-  return (
-  	<div id="landing-container">
-    	<nav role="navigation" className="nav-bar">
-            <h1 className="site-logo">DG</h1>
-            <Link to="#" className="sign-in-link">Sign in</Link>
+export default class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        listings: []
+    }
+  }
+  componentDidMount() {
+    fetch('http://localhost:8080/api/listings',
+    {
+      headers: {
+        'Accept': 'application/json',
+       // 'Content-Type': 'application/json',
+       // 'Authorization': `Bearer ${state.token}`
+      },
+      method: "GET"
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+      this.setState({
+        listings: responseJson
+      })
+    })
+    .catch(err => {
+      console.log("Whoops! Try this again.")
+    })
+  }
+  render() {
+    console.log(this.state.listings)
+    return (
+      <div id="landing-container">
+        <nav role="navigation" className="nav-bar">
+          <h1 className="site-logo">DG</h1>
+          <Link to="#" className="sign-in-link">Sign in</Link>
         </nav>
         <header role="banner">
-    		<h2 className="page-title">DoGood</h2>
-			<p id="app-description">DoGood is your one and only stop to find and list community service opportunities.</p>
-			<Link to="/signup" id="sign-up-link" className="button-style">Sign up to post a position</Link>
-    	</header>
-    	<main role="main">
-    		<h3 id="search-bar-label">Search</h3>
-    		<input type="text" placeholder="IPV, homelessness..." id="search-bar"></input>
-    		<section role="region" id="search-results">
-    			<div className="result">
-    				<span className="org-name">Org 1</span><span className="position-title">Title</span><span className="post-date">Date</span>
-    			</div>
-    			<div className="result">
-    				<span className="org-name">Org 2</span><span className="position-title">Title</span><span className="post-date">Date</span>
-    			</div>
-    			<div className="result">
-    				<span className="org-name">Org 3</span><span className="position-title">Title</span><span className="post-date">Date</span>
-    			</div>
-    		</section>
-    	</main>
-  	</div>
-  );
+          <h2 className="page-title">DoGood</h2>
+    			<p id="app-description">DoGood is your one and only stop to find and list community service opportunities.</p>
+    			<Link to="/signup" id="sign-up-link" className="button-style">Sign up to post a position</Link>
+        </header>
+        <main role="main">
+        	<h3 id="search-bar-label">Search</h3>
+        	<input type="text" placeholder="IPV, homelessness..." id="search-bar"></input>
+        	<section role="region" id="search-results">
+          {this.state.listings.map((listing, index) => (
+            <div className="result" key={index}>
+              <span className="org-name">{listing.title}</span><span className="position-title">{listing.title}</span><span className="post-date">{listing.date}</span>
+            </div>
+          ))}
+        	</section>
+        </main>
+      </div>
+    );
+  }
 }
