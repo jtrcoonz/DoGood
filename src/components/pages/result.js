620d2331050e0df2164de413197c4b499d4570cd
Sebastian Bastidas <sbastidasr@gmail.com>
2019-03-25 15:43:52 -0500

    Display details

--- src/components/pages/result.js
+++ src/components/pages/result.js
@@ -1,18 +1,43 @@
 import React from "react";
 
 export default class Result extends React.Component {
-    render() {
-        return(
-            <div className="result">
-                <span className="org-name">{this.props.listing.title}</span>
-                <span className="position-title">{this.props.listing.title}</span>
-                <span className="post-date">{this.props.listing.date}</span>
-                    <div className="result-details">
-                        <span className="result-category">{this.props.listing.category}</span>
-                        <span className="result-location">{this.props.listing.location}</span>
-                        <span className="result-description">{this.props.listing.description}</span>
-                    </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDetails: false
+    };
+  }
+
+  toggleDetails() {
+    this.setState({
+      displayDetails: !this.state.displayDetails
+    });
+  }
+
+  render() {
+    return (
+      <div className="result">
+        <span className="org-name">{this.props.listing.title}</span>
+        <span className="position-title">{this.props.listing.title}</span>
+        <span className="post-date">{this.props.listing.date}</span>
+        <button onClick={() => this.toggleDetails()}>
+          {this.state.displayDetails ? "close" : "open"}
+        </button>
+
+        {this.state.displayDetails ? (
+          <div className="result-details">
+            <span className="result-category">
+              {this.props.listing.category}
+            </span>
+            <span className="result-location">
+              {this.props.listing.location}
+            </span>
+            <span className="result-description">
+              {this.props.listing.description}
+            </span>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
