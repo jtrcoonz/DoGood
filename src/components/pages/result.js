ea3ed26c9ca1c8e58302e2cc05d1a6a7ce2953cf
Jacob Coons <jacobtcoons@gmail.com>
2019-11-08 23:53:53 -0800

    attempting some styling tricks with media queries and jsx

--- src/components/pages/result.js
+++ src/components/pages/result.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../pages-styling/result.css";
 import { API_BASE_URL } from "../../config";
+import { NONAME } from "dns";
 
 export default class Result extends React.Component {
   constructor(props) {
@@ -32,16 +33,22 @@ export default class Result extends React.Component {
   }
 
   render() {
-    const deleteButton = this.props.deleteButton == 'x' 
-                       ? <span className="delete-button" onClick={this.deleteResult}>{this.props.deleteButton}</span>
+    const deleteButton1 = this.props.deleteButton == 'x' 
+                       ? <span className="delete-button" id="delete-button-1" onClick={this.deleteResult}>{this.props.deleteButton}</span>
                        : '';
+    const deleteButton2 = this.props.deleteButton == 'x' 
+                       ? <span className="delete-button" id="delete-button-2" onClick={this.deleteResult}>{this.props.deleteButton}</span>
+                       : '';
+    const hidden = {
+      display: 'none' 
+    };
     return (
       <div className="result">
         <div className="result-basic">
           <span className="org-name">{this.props.listing.user.organizationName}</span>
           <span className="position-title"><a href = {`https://${this.props.listing.applyLink}`} target="_blank">{this.props.listing.title}</a></span>
           <span className="post-date">{this.props.listing.date.toString().slice(0, 10)}</span>
-          {deleteButton}
+          {deleteButton1}
           <button className="drop-arrow" onClick={() => this.toggleDetails()}>
             {this.state.displayDetails ? "▲" : "▼"}
           </button>
@@ -65,6 +72,12 @@ export default class Result extends React.Component {
               <span>: </span>
               {this.props.listing.description}
             </span>
+            <span className="result-delete-small-px" style={hidden}>
+              <span className="detail-header">Delete</span>
+              <span>: </span>
+              {deleteButton2}
+            </span>
+            
           </div>
         ) : null}
       </div>
